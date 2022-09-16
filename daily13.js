81f104eb563eca80230e553edef47b8cef3d2317
Vanessa Emerick <vavarooks123@gmail.com>
2022-09-16 09:10:13 -0700

    new algos

---
+++ daily13.js
@@ -0,0 +1,18 @@
+// Example for deduping sorted array: 
+// dedupe of  [ 1, 1, 1, 3, 4, 4, 4, 7, 8, 8 ] will be  [ 1, 3, 4, 7, 8 ]
+
+// Example for deduping of unsorted array:
+
+// Dedupe of [ 3, 4, 1, 4, 4, 5, 3, 4 ] will be [ 1, 3, 4, 5 ]
+
+function dup(arr) {
+    var nwarr= [arr[0]];
+    for (var i = 0 ; i < arr.length-1 ; i ++) {
+        if(arr[i] != arr[i+1]){
+            nwarr.push(arr[i+1])
+        }
+    } 
+    return nwarr
+}
+
+console.log(dup([ 1, 1, 1, 3, 4, 4, 4, 7, 8, 8 ]  ))
